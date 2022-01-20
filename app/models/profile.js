aa496628d6ab140bc591cb6139ea52498d125d7e
TheLenaburger <austin.lenaburg@gmail.com>
2022-01-19 20:04:20 -0600

    Fixed typo in profile_routes.js, updated instances of example with profile in profile.js and server.js

--- app/models/profile.js
+++ app/models/profile.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const exampleSchema = new mongoose.Schema({
+const profileSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
@@ -18,4 +18,4 @@ const exampleSchema = new mongoose.Schema({
   timestamps: true
 })
 
-module.exports = mongoose.model('Example', exampleSchema)
+module.exports = mongoose.model('Profile', profileSchema)
